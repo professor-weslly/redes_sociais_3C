51ccd25d1e9c2c6d1bfb8cc4559e3172b3a0f39b
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-11-06 07:59:06 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,6 +8,15 @@ async function visualizarInformacoesGlobais() {
   const horas = parseInt(dados.tempo_medio);
   const minutos = Math.round((dados.tempo_medio - horas) * 100);
   const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente<span>${pessoasConectadas} bilhões</span>
+estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> conectadas
+e <span>${minutos}</span>.<br>
+Isso significa que aproximadamente<span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.
+
+
 }
 
 visualizarInformacoesGlobais();
